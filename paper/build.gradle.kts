a8fed6a0d5e6abcf91a395444c0e9d9bef584662
adamekk04 <157727680+adamekk04@users.noreply.github.com>
2026-08-11 15:29:54 +0200

    Updated gradle wrapper and added shadow plugin
    Updated gradle wrapper from 9.6.0 to 9.6.1
    Added shadow plugin, because paper was building without one

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     id("java")
     alias(libs.plugins.run.paper)
+    alias(libs.plugins.shadow)
 }
 
 dependencies {
     implementation(project(":common"))
 
     compileOnly(libs.paper.api)
+}
+
+tasks.named("build") {
+    dependsOn("shadowJar")
 }
