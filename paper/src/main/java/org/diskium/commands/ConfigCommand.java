2aca99b7f8923fb4b53355d5c75e063c865b0465
adamekk04 <157727680+adamekk04@users.noreply.github.com>
2026-08-11 20:35:32 +0200

    Rename CommandManager to CommandManagement

--- paper/src/main/java/org/diskium/commands/ConfigCommand.java
+++ paper/src/main/java/org/diskium/commands/ConfigCommand.java
@@ -3,7 +3,7 @@ package org.diskium.commands;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import org.diskium.management.CommandManager;
+import org.diskium.management.CommandManagement;
 
 import java.util.List;
 
@@ -11,7 +11,7 @@ public class ConfigCommand {
 
     public static LiteralArgumentBuilder<CommandSourceStack> entry() {
 
-        List<LiteralArgumentBuilder<CommandSourceStack>> subcommands = CommandManager.configs();
+        List<LiteralArgumentBuilder<CommandSourceStack>> subcommands = CommandManagement.configs();
         LiteralArgumentBuilder<CommandSourceStack> configRoot = Commands.literal("config");
 
         for (LiteralArgumentBuilder<CommandSourceStack> subcommand : subcommands) {
