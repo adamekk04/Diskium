895e1d16a1eb8f353222bd0e02f3213e5af1207b
adamekk04 <157727680+adamekk04@users.noreply.github.com>
2026-09-15 17:27:49 +0200

    Fixed NPE in /diskium task list

--- common/src/main/java/org/diskium/utils/TasksUtils.java
+++ common/src/main/java/org/diskium/utils/TasksUtils.java
@@ -30,14 +30,16 @@ public class TasksUtils {
                 File path = new File(pathLine);
                 String mode = br.readLine();
 
-                if (mode.equals("1")) {
+                if (mode == null) {
+                    return tasks.toArray(TaskObj[]::new);
+                } else if (mode.equals("1")) {
                     tasks.add(new TaskObj(true, path, null, getType(path)));
                 } else {
                     tasks.add(new TaskObj(false, path, new File(mode), getType(path)));
                 }
             }
 
-            return tasks.toArray(new TaskObj[0]);
+            return tasks.toArray(TaskObj[]::new);
         } catch (SecurityException e) {
             MultiplatformLogger.error("Cannot access tasks.txt due to security reasons.");
         } catch (NoSuchFileException e) {
@@ -63,7 +65,7 @@ public class TasksUtils {
                 backups.add(new BackupObj(path, itself));
             }
 
-            return backups.toArray(new BackupObj[0]);
+            return backups.toArray(BackupObj[]::new);
         } catch (SecurityException e) {
             MultiplatformLogger.error("Cannot access backups.txt due to security reasons.");
         } catch (NoSuchFileException e) {
